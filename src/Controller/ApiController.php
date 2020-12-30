4532b7a49e54f8313786facbd05c1520fd8a6852
MassiBelaid <belaidmassinissamo@gmail.com>
2020-12-30 12:29:44 +0100

    add delete action to CRUD

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Character;
+use Doctrine\DBAL\Schema\View;
+use App\Repository\CharacterRepository;
 use App\Service\CharactersSerialization;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,4 +24,5 @@ class ApiController extends AbstractController
 
         return new JsonResponse(['data' => $serializedCharacters, 'items' => count($serializedCharacters)]);
     }
+
 }
